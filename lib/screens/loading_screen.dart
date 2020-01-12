639bb3de6d8b120b8e60bb85fa9ca4651aa00967
Evgeniy Mironov <wizard2014@ya.ru>
2020-01-12 23:21:36 +0200

    weather api added

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,11 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
+import 'location_screen.dart';
+import '../services/networking.dart';
 import '../services/location.dart';
+
+const apiKey = 'VALID_API_KEY';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,40 +17,38 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
 
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
+
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
 
-  void getData() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
+    var weatherData = await networkHelper.getData();
 
-      var decodedData = jsonDecode(data);
-
-      String city = decodedData['name'];
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-
-      print(city);
-      print(temperature);
-      print(condition);
-    } else {
-      print(response.statusCode);
-    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(
+          locationWeather: weatherData,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
